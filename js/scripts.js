cba49abdcab7df96ff981ad49ea5cde233e0fc73
MariaEripete <mariaichor@gmail.com>
2021-02-20 13:28:50 +0300

    add functions

--- js/scripts.js
+++ js/scripts.js
@@ -2,5 +2,24 @@ const getName=()=>{
     let name= document.getElementById("yourName").value;
     console.log (name)
 }
+let yearOfBirth=()=> {
+    let yob= document.getElementById("yearBorn").value;
+    console.log (yob)
+}
+
+let monthOfBirth=()=> {
+    let mm= document.getElementById("monthBorn").value;
+    console.log (mm)
+}
+let dayOfBirth=()=>{
+    let dd= document.getElementById("dayBorn").value;
+    console.log (dd)
+}
+let gender=()=>{
+    let gen= document.getElementById("male").value;
+    let gen1= document.getElementById("female").value;
+    console.log(gen || gen1)
+
+}
 
 
