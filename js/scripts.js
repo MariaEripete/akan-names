0164fdae3446f0f0284b43ac6b6d5a29512d6bec
MariaEripete <mariaichor@gmail.com>
2021-02-20 22:19:34 +0300

    add if else statements

--- js/scripts.js
+++ js/scripts.js
@@ -1,19 +1,23 @@
 const getName=()=>{
     let name= document.getElementById("yourName").value;
-    console.log (name)
 }
 let yearOfBirth=()=> {
     let yob= document.getElementById("yearBorn").value;
-    console.log (yob)
+    var yy= parseInt(yob.toString().slice(0, 2))
+    var cc= parseInt(yob.toString().slice(2))
 }
 
 let monthOfBirth=()=> {
     let mm= document.getElementById("monthBorn").value;
-    console.log (mm)
+    if (mm<=0 || mm>=12 = true){
+        return("Invalid month")
+    }
 }
 let dayOfBirth=()=>{
     let dd= document.getElementById("dayBorn").value;
-    console.log (dd)
+    if (dd<=0 || dd>31 = true){
+        return("Invalid day")
+    }
 }
 let gender=()=>{
     let gen= document.getElementById("male").value;
@@ -21,5 +25,9 @@ let gender=()=>{
     console.log(gen || gen1)
 
 }
+var results = parseInt(( ( (cc/4) -2*cc-1) + ((5*yy/4) ) + ((26*(MM+1)/10)) + DD ) % 7);
+
+var mNames=["Kwasi","Kwadwo","Kwabena","Kwaku","Yaw","Kofi","Kwame"]
+var fNames=["Akosua","Adwoa","Abenaa","Akua","Yaa","Afua","Ama"]
 
 
