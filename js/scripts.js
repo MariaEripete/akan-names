6cde2e3908e24eb2b2c1e90f98bec1c235c070d0
MariaEripete <mariaichor@gmail.com>
2021-02-22 15:10:36 +0300

    update javascript

--- js/scripts.js
+++ js/scripts.js
@@ -90,8 +90,9 @@ let akan=()=>{
     
 }
  var output = ()=>{
-     answer. innerHTML= "YourAkan name is "+ 
+     answer. innerHTML= "YourAkan name is "+ answer;
  }
+ 
 
 
 
@@ -101,38 +102,5 @@ let akan=()=>{
 
 
 
-
-
-let yearOfBirth=()=> {
-    let yob= document.getElementById("yearBorn").value;
-    var yy= parseInt(yob.toString().slice(0, 2))
-    var cc= parseInt(yob.toString().slice(2))
-}
-
-let monthOfBirth=()=> {
-    let mm= document.getElementById("monthBorn").value;
-    if (mm<=0 || mm>=12) = true;{
-        return("Invalid month")
-    }
-}
-let dayOfBirth=()=>{
-    let dd= document.getElementById("dayBorn").value;
-    try{
-        if (dd<=0 || dd>31) throw "Invalid day";
-    }
-    catch(err){
-        message.innerHTML = err;
-    }
-}
-let gen=()=>{
-    let gen= document.getElementById("male").value;
-    let gen1= document.getElementById("female").value;
-    console.log(gen || gen1)
-
-}
-var results = parseInt(( ( (cc/4) -2*cc-1) + ((5*yy/4) ) + ((26*(MM+1)/10)) + DD ) % 7);
-
-var mNames=["Kwasi","Kwadwo","Kwabena","Kwaku","Yaw","Kofi","Kwame"]
-var fNames=["Akosua","Adwoa","Abenaa","Akua","Yaa","Afua","Ama"]
 
 
